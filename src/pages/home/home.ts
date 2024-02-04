ff39d501166a35e4ddf7b04fa746d37a946916d8
Louvel-Marvin <155963514+Louvel-Marvin@users.noreply.github.com>
2024-02-04 20:41:22 +0100

    Un peu plus propre, mais toujours pas de quoi se vanter.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,9 +19,15 @@ const url = "https://api.themoviedb.org/3/trending/movie/week?api_key=5f25c2d976
       movies.forEach(movie => {
         moviesHTML += `
           <div class=" " >
+          <div class="mb-4 mt-4 ml-4 mr-4 text-center text-2xl font-bold text-blue-500">
             <h2>${movie.title}</h2>
-            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-            <p>${movie.overview}</p>
+            </div>
+            <div class="flex justify-center">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" id="moviePoster">
+            </div>
+            <div class="mt-8">
+            <p class="flex justify-center text-lg">${movie.overview}</p>
+            </div>
             <a href="../movie/movie.html?id=${movie.id}">Voir plus</a>
           </div>
         `;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,html}",
     "./src/pages/**/*.{js,ts,jsx,tsx,html}",
     "./home/**/*.{js,ts,jsx,tsx,html}",
-    "./home/home.html",
   ],
   purge: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,html}",
     "./home/**/*.{js,ts,jsx,tsx,html}",
-    "./home/home.html",
   ],
   theme: {
     extend: {},
